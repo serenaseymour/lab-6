b1e0ade246e80e66995aff4c9f20415e138170e8
serenaseymour <116756446+serenaseymour@users.noreply.github.com>
2022-10-27 09:50:24 -0400

    Update add_numbers.py

--- add_numbers.py
+++ add_numbers.py
@@ -1,5 +1,19 @@
 num1 = int(input("Input the first number:"))
 num2 = int(input("Input the second number:"))
 
-num3 = num1 + num2
-print(f"{num1} + {num2} = {num3}")
+print("Choose an option:")
+print("1. Add")
+print("2. Subtract")
+
+option = int(input())
+
+if option == 1:
+  num3 = num1 + num2
+  print(f"{num1} + {num2} = {num3}")
+
+elif option == 2:
+  num3 = num1 - num2
+  print(f"{num1} - {num2} = {num3}")
+  
+else:
+  print("Error: Invalid selction")
